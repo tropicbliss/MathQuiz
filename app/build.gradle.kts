9ba35df012a9d4466e81568467ae39e796fcd77c
tropicbliss <tropicbliss@protonmail.com>
2023-09-04 17:20:38 +0800

    Prepare app for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,10 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isDebuggable = false
         }
     }
     compileOptions {
